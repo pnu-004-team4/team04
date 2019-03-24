4a74b5844f138b3cba79a3ad3575bb67959c102d
Chang-Sub Kim <peurocs4@gmail.com>
2019-03-24 21:01:53 +0900

    Version 1.00b

---
+++ src/main/java/com/team04/musiccloud/MusiccloudApplication.java
@@ -0,0 +1,13 @@
+package com.team04.musiccloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusiccloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusiccloudApplication.class, args);
+    }
+
+}
