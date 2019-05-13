37cc64e942ac945812bbcdfa1123a99c241d14f0
BlaCkinkGJ <ss5kijun@gmail.com>
2019-05-14 01:13:42 +0900

    API 키가 외부로 노출 되는 문제 해결
    API 키가 외부로 노출되는 문제를 발견하여 수정하였습니다. 프로그램을 실행을 할 때에 매개변수로 API 키를 부여하면 이전과 동일하게 사용할 수 있습니다. 그리고 이전의 키 값이 원격 저장소에서 남는 것을 방지하기 위해 `git rm --cached`를 수행했습니다.

--- src/main/java/com/team04/musiccloud/MusiccloudApplication.java
+++ src/main/java/com/team04/musiccloud/MusiccloudApplication.java
@@ -1,9 +1,11 @@
 package com.team04.musiccloud;
 
+import com.beust.jcommander.ParameterException;
 import com.team04.musiccloud.audio.extractor.ExtractorException;
 import com.team04.musiccloud.audio.extractor.InvalidFileFormat;
 import com.team04.musiccloud.stream.caching.AudioCaching;
 import com.team04.musiccloud.tester.Tester;
+import com.team04.musiccloud.utilities.StaticKeys;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import org.springframework.boot.SpringApplication;
@@ -12,7 +14,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MusiccloudApplication {
 
-  public static void main(String[] args) throws IOException, ExtractorException, InvalidFileFormat {
+  public static void main(String[] args)
+      throws IOException, ExtractorException, InvalidFileFormat, ParameterException {
     SpringApplication.run(MusiccloudApplication.class, args);
 
     // 2019년 4월 19일 추가 ==> 5분 단위로 GC를 수행하도록 합니다.
@@ -20,6 +23,11 @@ public class MusiccloudApplication {
     audioCaching.setPeriod(5); // 비기능적 요구사항 수행
     audioCaching.setTimeUnit(TimeUnit.MINUTES);
     audioCaching.start();
+
+    if (args.length != 1) {
+      throw new ParameterException("You must have the Mongo DB API Keys");
+    }
+    StaticKeys.setKeys(args[0]);
 
     // @TODO: 삭제하도록 할 것
     Tester.testUploader();
