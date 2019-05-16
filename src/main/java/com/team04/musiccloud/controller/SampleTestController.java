434d435cc98776cd0286a3e98b56110312fd811e
myulchkun <steven800@naver.com>
2019-05-17 07:29:08 +0900

    code smell 제거
    code smell 제거

--- src/main/java/com/team04/musiccloud/controller/SampleTestController.java
+++ src/main/java/com/team04/musiccloud/controller/SampleTestController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class SampleTestController {
     
