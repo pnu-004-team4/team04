b4f4aa9a863c4e643455ddeb2365b5760074c976
myulchkun <steven800@naver.com>
2019-05-17 07:28:48 +0900

    사용자 권한에 따른 폴더 접근 기능 구현
    ADMIN 권한을 가진 사용자만 CSK폴더의 파일에 접근 가능

--- src/main/java/com/team04/musiccloud/auth/WebSecurityConfig.java
+++ src/main/java/com/team04/musiccloud/auth/WebSecurityConfig.java
@@ -12,7 +12,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // [[[[ 주의 ]]]] 아래 코드는 로그인 인증과 관련된 코드로 혹여 권한 오류 생길 시 확인바랍니다.
-        Account account = new Account();
         http.authorizeRequests()
                 .antMatchers("/login", "/register", "/css/**", "/js/**", "/registerCheck").permitAll()
                 //.antMatchers("/server/storage/" + account.getEmail() + "/**").hasRole("" + account.getEmail())
