089d3778760370f5ac7b797b26db5936dc70a272
BlaCkinkGJ <ss5kijun@gmail.com>
2019-05-20 14:20:44 +0900

    파일의 Directory를 지정된 위치에서만 가져오도록 수정
    파일의 Directory를 지정된 위치에서만 가져오도록 수정하기 위해 FileMetaConverter를 수정하도록 했습니다. 이외에 오류를 발생시키는 test를 수정하도록 하였습니다.

--- src/main/java/com/team04/musiccloud/db/converter/FileMetaConverter.java
+++ src/main/java/com/team04/musiccloud/db/converter/FileMetaConverter.java
@@ -2,39 +2,41 @@ package com.team04.musiccloud.db.converter;
 
 import com.team04.musiccloud.audio.FileMeta;
 import com.team04.musiccloud.audio.FileMetaBuilder;
+import com.team04.musiccloud.utilities.StaticPaths;
 import org.bson.Document;
 import org.bson.types.ObjectId;
+
+import java.nio.file.Path;
 
 public class FileMetaConverter {
 
     public static Document toDocument(FileMeta fileMeta) {
-        Document document = new Document("_id", fileMeta.getDbId())
+
+        return new Document("_id", fileMeta.getDbId())
                 .append("directory", fileMeta.getDirectory())
                 .append("name", fileMeta.getName())
                 .append("extension", fileMeta.getExtension())
                 .append("user", fileMeta.getUser());
-
-        return document;
     }
 
     public static Document toDocument(FileMeta fileMeta, String dbId) {
-        Document document = new Document("_id", new ObjectId(dbId))
+
+        return new Document("_id", new ObjectId(dbId))
                 .append("directory", fileMeta.getDirectory())
                 .append("name", fileMeta.getName())
                 .append("extension", fileMeta.getExtension())
                 .append("user", fileMeta.getUser());
-
-        return document;
     }
 
     public static FileMeta toFileMeta(Document document) {
-        FileMeta fileMeta = FileMetaBuilder.builder()
+        String userName = document.get("name").toString();
+        Path filePath = StaticPaths.storage.resolve(userName).toAbsolutePath();
+
+        return FileMetaBuilder.builder()
                 .setDbId(document.get("_id").toString())
-                .setDirectory((String) document.get("directory"))
-                .setName((String) document.get("name"))
+                .setDirectory(filePath.toString())
+                .setName(userName)
                 .setExtension((String) document.get("extension"))
                 .setUser((String) document.get("user")).build();
-
-        return fileMeta;
     }
 }
