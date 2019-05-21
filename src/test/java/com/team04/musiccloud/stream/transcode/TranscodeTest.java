ae4ce2f2962056f79e4828f0a7ad89d6e7d24fe4
BlaCkinkGJ <ss5kijun@gmail.com>
2019-05-22 01:19:05 +0900

    테스트 파일을 개선하였습니다.
    AudioTestHandlerTest는 이전과 동일하게 동작하지 아니하나 혹시 모를 Compile 에러를 방지하기 위해서 수정했습니다. 그리고 StreamingTest 및 TranscodeTest를 개선하였습니다.

--- src/test/java/com/team04/musiccloud/stream/transcode/TranscodeTest.java
+++ src/test/java/com/team04/musiccloud/stream/transcode/TranscodeTest.java
@@ -2,21 +2,19 @@ package com.team04.musiccloud.stream.transcode;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import be.hogent.tarsos.transcoder.DefaultAttributes;
-import com.fasterxml.jackson.databind.deser.std.DelegatingDeserializer;
 import com.team04.musiccloud.audio.Audio;
 import com.team04.musiccloud.audio.extractor.AudioExtractor;
 import com.team04.musiccloud.audio.extractor.Mp3Extractor;
-import com.team04.musiccloud.controller.PlayerController;
-
+import com.team04.musiccloud.utilities.StaticPaths;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-
-import com.team04.musiccloud.utilities.StaticPaths;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,22 +23,30 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class TranscodeTest {
 
-  private Transcode transcode;
   private static Path cacheDirectory = StaticPaths.tempStorage;
   private static Path storageDirectory = StaticPaths.storage;
+  private Transcode transcode;
 
   @Before
   public void setUp() throws Exception {
-    final String user = "CSK";
+    final String user = "admin@admin.com";
+    System.out.println(cacheDirectory.resolve(user).toString());
+    File directory = new File(cacheDirectory.resolve(user).toString());
+
+    if (!directory.exists()) {
+      assertTrue(directory.mkdirs());
+    }
+
     Files.copy((storageDirectory.resolve(user)).resolve("sample.mp3")
-            , (cacheDirectory.resolve(user)).resolve("sample.mp3")
-            , StandardCopyOption.REPLACE_EXISTING);
+        , (cacheDirectory.resolve(user)).resolve("sample.mp3")
+        , StandardCopyOption.REPLACE_EXISTING);
+
     final Path currentLocation = cacheDirectory.resolve(user)
-            .resolve("sample.mp3").toAbsolutePath();
+        .resolve("sample.mp3").toAbsolutePath();
     final AudioExtractor extractor = new Mp3Extractor();
 
     MultipartFile myFile = new MockMultipartFile(currentLocation.toString(),
-            "sample.mp3", null, new FileInputStream(currentLocation.toFile()));
+        "sample.mp3", null, new FileInputStream(currentLocation.toFile()));
     extractor.setBaseDirectory(cacheDirectory);
     Audio audio = extractor.getAudio(myFile, user);
     transcode = new Transcode(audio);
