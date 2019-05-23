5be923fbc89e3c276157726bfeb16af91afe58d8
jkuot36 <jkuot36@naver.com>
2019-05-24 01:06:58 +0900

    파일 업로드 컨트롤러 초기 버전 작성

---
+++ src/main/java/com/team04/musiccloud/controller/UploadController.java
@@ -0,0 +1,43 @@
+package com.team04.musiccloud.controller;
+
+import com.team04.musiccloud.audio.AudioHandler;
+import com.team04.musiccloud.audio.extractor.ExtractorException;
+import com.team04.musiccloud.audio.extractor.InvalidFileFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Controller
+public class UploadController {
+    private final Logger logger = LoggerFactory.getLogger(UploadController.class);
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public ResponseEntity<?> uploadFile(
+            @RequestParam("file") MultipartFile uploadFile) throws ExtractorException, IOException, InvalidFileFormat {
+
+        if (uploadFile.isEmpty()) {
+            return new ResponseEntity("Please select a File!", HttpStatus.OK);
+        }
+
+        try {
+            //AudioHandler audioHandler = new AudioHandler(username);
+            AudioHandler audioHandler = new AudioHandler("CSK");
+            audioHandler.requestUpload(uploadFile);
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity(":Successfully uploaded - " +
+                uploadFile.getOriginalFilename(), new HttpHeaders(), HttpStatus.OK);
+    }
+}
