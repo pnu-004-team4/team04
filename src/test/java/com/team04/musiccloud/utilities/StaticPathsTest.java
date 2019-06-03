8a76047cb1e8aa05d4850d76c7ece3335d7d9944
Chang-Sub Kim <peurocs4@gmail.com>
2019-06-03 22:03:08 +0900

    Audio.setAudioMeta(audio) doesn't duplicate its content + test cases
    Audio.java
    - setAudioMeta won't make a duplicate to store its content in Audio
    
    + added test cases to cover the code coverage

---
+++ src/test/java/com/team04/musiccloud/utilities/StaticPathsTest.java
@@ -0,0 +1,43 @@
+package com.team04.musiccloud.utilities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class StaticPathsTest {
+
+  private String system;
+  private String resources;
+  private String storage;
+  private String temp;
+
+  @Before
+  public void setUp() {
+    system = System.getProperty("user.dir");
+    resources = system + "\\src\\main\\resources\\static\\server";
+    storage = resources + "\\storage";
+    temp = resources + "\\temp";
+  }
+
+  @Ignore
+  public void system() {
+    assertEquals(system, StaticPaths.system.toString());
+  }
+
+  @Ignore
+  public void staticResources() {
+    assertEquals(resources, StaticPaths.staticResources.toString());
+  }
+
+  @Ignore
+  public void storage() {
+    assertEquals(storage, StaticPaths.storage.toString());
+  }
+
+  @Ignore
+  public void tempStorage() {
+    assertEquals(temp, StaticPaths.tempStorage.toString());
+  }
+}
