ea324377cb813b138d9e27f9b33dcb0a744d48ab
mksoo <mgs3067@naver.com>
2019-06-07 16:45:56 +0900

    codacy 수정 및 디자인 보완

--- src/main/resources/static/js/loadingBar.js
+++ src/main/resources/static/js/loadingBar.js
@@ -7,9 +7,11 @@
 var canvas = document.getElementById("loading_bar");
 var context = canvas.getContext("2d");
 
+
 //캔버스 크기 -> 배경에 대한 원의 비율이 결정.
 canvas.width = 100;
 canvas.height = 100;
+
 
 //위치..
 var x = canvas.width / 2;
@@ -20,16 +22,19 @@ var radius = 35;
 
 // 색의 시작과 끝의 각도. 도는거
 var startAngle = 0 * Math.PI;
-var endAngle = 1.2 * Math.PI;
-var counterClockwise = true;
+var endAngle = 1 * Math.PI;
+var counterClockwise = false;
 
 //원 둘레의 반지름?
-context.lineWidth = 4;
+context.lineWidth = 2;
 //몰라 이건.
 context.strokeStyle = "white";
 
+var i = 0;
+var cMax = 255;
+
 //시작 색깔인듯
-var red = 255;
+var red = 0;
 var blue = 0;
 var green = 0;
 
@@ -39,37 +44,14 @@ var green = 0;
 
 //빙빙 도는 메소드
 var loop = function(){
-
     // 밝아지는 속도와 느려지는 속도
     startAngle = (startAngle + 0.025 * Math.PI)%(2*Math.PI);
     endAngle = (endAngle + 0.025 * Math.PI)%(2*Math.PI);
 
-    if(red >= 255) {
-        if(green > 0){
-            green -= 5;
-        }
-        else{
-            blue += 5;
-        }
-    }
-
-    if(blue >= 255) {
-        if(red > 0){
-            red -= 5;
-        }
-        else{
-            green += 5;
-        }
-    }
-    if(green >= 255) {
-        if(blue > 0){
-            blue -= 5;
-        }
-        else{
-            red += 5;
-        }
-    }
-
+    red = cMax + cMax*Math.sin(i*Math.PI/50);
+    blue = cMax + cMax*Math.sin(i*Math.PI/100);
+    green = cMax + cMax*Math.sin( i*Math.PI/150);
+    i += 1;
 //stroke 투명도
     context.strokeStyle = "rgba("+red+","+green+","+blue+",1)";
     context.beginPath();
@@ -79,11 +61,15 @@ var loop = function(){
 
     context.strokeStyle = "rgba(0,0,0,1)";
     context.beginPath();
-    context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+    context.arc(x, y, radius-0.8, startAngle, endAngle, counterClockwise);
     context.stroke();
     context.closePath();
 
-
+    context.strokeStyle = "rgba(0,0,0,1)";
+    context.beginPath();
+    context.arc(x, y, radius+0.8, startAngle, endAngle, counterClockwise);
+    context.stroke();
+    context.closePath();
 };
 
 setInterval(loop, 10);
