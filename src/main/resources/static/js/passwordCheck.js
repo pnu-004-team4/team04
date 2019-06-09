c52f3c515f2b53fe745994cb50d69426bd8a103d
jongjn <jjj0986@naver.com>
2019-06-10 03:02:29 +0900

    code smell 수정
    code smell 수정

--- src/main/resources/static/js/passwordCheck.js
+++ src/main/resources/static/js/passwordCheck.js
@@ -1,36 +1,36 @@
 var strength=0;
 const passwordLength = () => {
-    let inputVal = document.querySelector('#password').value;
-    const inputLen = document.querySelector('#inputLen');
-    document.querySelector('#percent').style.color = 'transparent';
+    let inputVal = document.querySelector("#password").value;
+    const inputLen = document.querySelector("#inputLen");
+    document.querySelector("#percent").style.color = 'transparent';
 
-    if (inputVal.includes(' ')){
-        inputLen.innerHTML  = 'Whitespaces are not allowed';
+    if (inputVal.includes(" ")){
+        inputLen.innerHTML  = "Whitespaces are not allowed";
         document.querySelector('.progress').style.display = 'none'
         return;
     }
 
     if (inputVal.length === 0) {
         inputLen.innerHTML = '';
-        document.querySelector('.progress').style.display = 'none';
+        document.querySelector('.progress').style.display = "none";
         return;
     }
 
     if (inputVal.length < 6) {
-        inputLen.innerHTML = 'Minimum password length is 6';
+        inputLen.innerHTML = "Minimum password length is 6";
         document.querySelector('.progress').style.display = 'none'
         return;
     }
 
     if (inputVal.length >= 6) {
-        inputLen.innerHTML = '';
-        document.querySelector('.progress').style.display = 'inline-block';
+        inputLen.innerHTML = "";
+        document.querySelector('.progress').style.display = "inline-block";
     }
 
     if(inputVal.length > 12) {
-        inputLen.innerHTML = 'Maximum password length is 12';
-        document.querySelector('.progress').style.display = 'none'
-        return
+        inputLen.innerHTML = "Maximum password length is 12";
+        document.querySelector('.progress').style.display = "none"
+        return;
     }
 
     const lower = /[a-z]/.test(inputVal);
@@ -55,41 +55,41 @@ const passwordLength = () => {
         document.querySelector('#percent').innerHTML = passStrength;
     }
     strength = passStrength;
-    document.querySelector('#percent').innerHTML += '%';
+    document.querySelector('#percent').innerHTML += "%";
 
 
-    let percentage = document.querySelector('#percent').innerHTML;
+    let percentage = document.querySelector("#percent").innerHTML;
 
-    if (percentage === '25%'){
-        document.querySelector('.progressBar').style.background = 'red';
+    if (percentage === "25%"){
+        document.querySelector('.progressBar').style.background = "red";
         document.querySelector('.progressBar').style.width = percentage;
     }
-    if (percentage === '50%'){
-        document.querySelector('.progressBar').style.background = '#FAD054';
+    if (percentage === "50%"){
+        document.querySelector('.progressBar').style.background = "#FAD054";
         document.querySelector('.progressBar').style.width = percentage;
     }
-    if (percentage === '75%'){
-        document.querySelector('.progressBar').style.background = 'blue';
+    if (percentage === "75%"){
+        document.querySelector('.progressBar').style.background = "blue";
         document.querySelector('.progressBar').style.width = percentage;
     }
-    if (percentage === '100%'){
-        document.querySelector('.progressBar').style.background = 'green';
+    if (percentage === "100%"){
+        document.querySelector('.progressBar').style.background = "green";
         document.querySelector('.progressBar').style.width = percentage;
     }
 
-}
-document.querySelector('#password').addEventListener('keyup', passwordLength);
+};
+document.querySelector("#password").addEventListener("keyup", passwordLength);
 
 
 function check() {
-    if($('#password').val() != '' && strength < 75){
-        alert('Password Security is too low.')
-        $('#password').focus();
+    if($('#password').val() != "" && strength < 75){
+        alert("Password Security is too low.");
+        $("#password").focus();
         return false;
     }else if(strength >= 75){
-        if($('#password').val() != $('#cpassword').val()) {
-            alert('Password does not match. Please check your password again')
-            $('#cpassword').focus();
+        if($("#password").val() != $("#cpassword").val()) {
+            alert("Password does not match. Please check your password again");
+            $("#cpassword").focus();
             return false;
         }
         return true;
