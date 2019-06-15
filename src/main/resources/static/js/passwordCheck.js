9ce9e22084044a3e2b217fa75e724c699ad0961f
BlaCkinkGJ <ss5kijun@gmail.com>
2019-06-15 15:36:54 +0900

    Update some files

--- src/main/resources/static/js/passwordCheck.js
+++ src/main/resources/static/js/passwordCheck.js
@@ -1,99 +1,100 @@
+var strength = 0;
+var passwordLength = 0;
 $(document).ready(function () {
-    var strength = 0;
-    var passwordLength = function () {
-        var inputVal = document.querySelector("#password").value;
-        var inputLen = document.querySelector("#inputLen");
-        document.querySelector("#percent").style.color = "transparent";
+  passwordLength = function () {
+    var inputVal = document.querySelector("#password").value;
+    var inputLen = document.querySelector("#inputLen");
+    document.querySelector("#percent").style.color = "transparent";
 
-        if (inputVal.includes(" ")) {
-            inputLen.innerHTML = "Whitespaces are not allowed";
-            document.querySelector(".progress").style.display = "none";
-            return;
-        }
+    if (inputVal.includes(" ")) {
+      inputLen.innerHTML = "Whitespaces are not allowed";
+      document.querySelector(".progress").style.display = "none";
+      return;
+    }
 
-        if (inputVal.length === 0) {
-            inputLen.innerHTML = "";
-            document.querySelector(".progress").style.display = "none";
-            return;
-        }
+    if (inputVal.length === 0) {
+      inputLen.innerHTML = "";
+      document.querySelector(".progress").style.display = "none";
+      return;
+    }
 
-        if (inputVal.length < 6) {
-            inputLen.innerHTML = "Minimum password length is 6";
-            document.querySelector(".progress").style.display = "none";
-            return;
-        }
+    if (inputVal.length < 6) {
+      inputLen.innerHTML = "Minimum password length is 6";
+      document.querySelector(".progress").style.display = "none";
+      return;
+    }
 
-        if (inputVal.length >= 6) {
-            inputLen.innerHTML = "";
-            document.querySelector(".progress").style.display = "inline-block";
-        }
+    if (inputVal.length >= 6) {
+      inputLen.innerHTML = "";
+      document.querySelector(".progress").style.display = "inline-block";
+    }
 
-        if (inputVal.length > 12) {
-            inputLen.innerHTML = "Maximum password length is 12";
-            document.querySelector(".progress").style.display = "none";
-            return;
-        }
+    if (inputVal.length > 12) {
+      inputLen.innerHTML = "Maximum password length is 12";
+      document.querySelector(".progress").style.display = "none";
+      return;
+    }
 
-        var lower = /[a-z]/.test(inputVal);
-        var upper = /[A-Z]/.test(inputVal);
-        var digit = /[0-9]/.test(inputVal);
-        var specialChar = /[$@#&!`~%^*()=+_,.<>/?-]/.test(inputVal);
-        var passStrength = 0;
-        if (lower) {
-            passStrength += 25;
-            document.querySelector("#percent").innerHTML = passStrength;
-        }
-        if (upper) {
-            passStrength += 25;
-            document.querySelector("#percent").innerHTML = passStrength;
-        }
-        if (digit) {
-            passStrength += 25;
-            document.querySelector("#percent").innerHTML = passStrength;
-        }
-        if (specialChar) {
-            passStrength += 25;
-            document.querySelector("#percent").innerHTML = passStrength;
-        }
-        strength = passStrength;
-        document.querySelector("#percent").innerHTML += "%";
+    var lower = /[a-z]/.test(inputVal);
+    var upper = /[A-Z]/.test(inputVal);
+    var digit = /[0-9]/.test(inputVal);
+    var specialChar = /[$@#&!`~%^*()=+_,.<>/?-]/.test(inputVal);
+    var passStrength = 0;
+    if (lower) {
+      passStrength += 25;
+      document.querySelector("#percent").innerHTML = passStrength;
+    }
+    if (upper) {
+      passStrength += 25;
+      document.querySelector("#percent").innerHTML = passStrength;
+    }
+    if (digit) {
+      passStrength += 25;
+      document.querySelector("#percent").innerHTML = passStrength;
+    }
+    if (specialChar) {
+      passStrength += 25;
+      document.querySelector("#percent").innerHTML = passStrength;
+    }
+    strength = passStrength;
+    document.querySelector("#percent").innerHTML += "%";
 
-        var percentage = document.querySelector("#percent").innerHTML;
+    var percentage = document.querySelector("#percent").innerHTML;
 
-        if (percentage === "25%") {
-            document.querySelector(".progressBar").style.background = "red";
-            document.querySelector(".progressBar").style.width = percentage;
-        }
-        if (percentage === "50%") {
-            document.querySelector(".progressBar").style.background = "#FAD054";
-            document.querySelector(".progressBar").style.width = percentage;
-        }
-        if (percentage === "75%") {
-            document.querySelector(".progressBar").style.background = "blue";
-            document.querySelector(".progressBar").style.width = percentage;
-        }
-        if (percentage === "100%") {
-            document.querySelector(".progressBar").style.background = "green";
-            document.querySelector(".progressBar").style.width = percentage;
-        }
+    if (percentage === "25%") {
+      document.querySelector(".progressBar").style.background = "red";
+      document.querySelector(".progressBar").style.width = percentage;
+    }
+    if (percentage === "50%") {
+      document.querySelector(".progressBar").style.background = "#FAD054";
+      document.querySelector(".progressBar").style.width = percentage;
+    }
+    if (percentage === "75%") {
+      document.querySelector(".progressBar").style.background = "blue";
+      document.querySelector(".progressBar").style.width = percentage;
+    }
+    if (percentage === "100%") {
+      document.querySelector(".progressBar").style.background = "green";
+      document.querySelector(".progressBar").style.width = percentage;
+    }
 
-    };
-    document.querySelector("#password").addEventListener("keyup",
-        passwordLength);
+  };
+  document.querySelector("#password").addEventListener("keyup",
+      passwordLength);
 });
 
 function check() {
-    if ($("#password").val() !== "" && strength < 75) {
-        alert("Password Security is too low.");
-        $("#password").focus();
-        return false;
-    } else if (strength >= 75) {
-        if ($("#password").val() !== $("#cpassword").val()) {
-            alert(
-                "Password does not match. Please check your password again");
-            $("#cpassword").focus();
-            return false;
-        }
-        return true;
+  if ($("#password").val() !== "" && strength < 75) {
+    alert("Password Security is too low.");
+    $("#password").focus();
+    return false;
+  } else if (strength >= 75) {
+    if ($("#password").val() !== $("#cpassword").val()) {
+      alert(
+          "Password does not match. Please check your password again");
+      $("#cpassword").focus();
+      return false;
     }
+    return true;
+  }
 }
--- src/main/webapp/WEB-INF/jsp/Registration/registerCheck.jsp
+++ src/main/webapp/WEB-INF/jsp/Registration/registerCheck.jsp
@@ -11,6 +11,7 @@
 <html lang="kr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>register</title>
     <link rel="stylesheet" href="/css/login.css">
     <style rel="stylesheet">
