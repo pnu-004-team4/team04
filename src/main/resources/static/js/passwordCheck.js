2ea071cdbbf57f45ffc3ee630040550a2cafba90
BlaCkinkGJ <ss5kijun@gmail.com>
2019-06-15 16:30:56 +0900

    Lower CC

--- src/main/resources/static/js/passwordCheck.js
+++ src/main/resources/static/js/passwordCheck.js
@@ -5,38 +5,45 @@ var globalPasswordLength = 0;
 var MIN_PWD_LEN = 6;
 var MAX_PWD_LEN = 12;
 
+function checkTheEmpty(inputLen, passwordLength) {
+  if (passwordLength !== 0) {
+    inputLen.innerText = "Minimum password length is " + MIN_PWD_LEN;
+  } else {
+    inputLen.innerText = "";
+  }
+}
+
 function checkTheLength(inputVal, inputLen) {
   if (inputVal.includes(" ")) {
-    inputLen.innerHTML = "Whitespaces are not allowed";
+    inputLen.innerText = "Whitespaces are not allowed";
     document.querySelector(".progress").style.display = "none";
     return false;
   }
 
   globalPasswordLength = inputVal.length;
 
-  if (globalPasswordLength === 0) {
-    inputLen.innerHTML = "";
+  if (globalPasswordLength > MAX_PWD_LEN) {
+    inputLen.innerText = "Maximum password length is " + MAX_PWD_LEN;
     document.querySelector(".progress").style.display = "none";
     return false;
-  }
-
-  if (globalPasswordLength < MIN_PWD_LEN) {
-    inputLen.innerHTML = "Minimum password length is " + MIN_PWD_LEN;
+  } else if (globalPasswordLength < MIN_PWD_LEN) {
+    checkTheEmpty(inputLen, globalPasswordLength);
     document.querySelector(".progress").style.display = "none";
     return false;
-  }
-
-  if (globalPasswordLength >= MIN_PWD_LEN) {
-    inputLen.innerHTML = "";
+  } else {
+    inputLen.innerText = "";
     document.querySelector(".progress").style.display = "inline-block";
   }
 
-  if (globalPasswordLength > MAX_PWD_LEN) {
-    inputLen.innerHTML = "Maximum password length is " + MAX_PWD_LEN;
-    document.querySelector(".progress").style.display = "none";
-    return false;
-  }
   return true;
+}
+
+function checkPassStrengthStatus(state, passStrength) {
+  if (state) {
+    document.querySelector("#percent").innerText = String(passStrength);
+    return 25;
+  }
+  return 0;
 }
 
 function getPassStrength(inputVal) {
@@ -45,42 +52,27 @@ function getPassStrength(inputVal) {
   var digit = /[0-9]/.test(inputVal);
   var specialChar = /[$@#&!`~%^*()=+_,.<>/?-]/.test(inputVal);
   var passStrength = 0;
-  if (lower) {
-    passStrength += 25;
-    document.querySelector("#percent").innerHTML = String(passStrength);
-  }
-  if (upper) {
-    passStrength += 25;
-    document.querySelector("#percent").innerHTML = String(passStrength);
-  }
-  if (digit) {
-    passStrength += 25;
-    document.querySelector("#percent").innerHTML = String(passStrength);
-  }
-  if (specialChar) {
-    passStrength += 25;
-    document.querySelector("#percent").innerHTML = String(passStrength);
-  }
+
+  passStrength += checkPassStrengthStatus(lower, passStrength);
+  passStrength += checkPassStrengthStatus(upper, passStrength);
+  passStrength += checkPassStrengthStatus(digit, passStrength);
+  passStrength += checkPassStrengthStatus(specialChar, passStrength);
+
   return passStrength;
 }
 
+function drawSecurityPercentage(basePercentage, percentage, color) {
+  if (percentage === basePercentage) {
+    document.querySelector(".progressBar").style.background = color;
+    document.querySelector(".progressBar").style.width = percentage;
+  }
+}
+
 function percentageCheck(percentage) {
-  if (percentage === "25%") {
-    document.querySelector(".progressBar").style.background = "red";
-    document.querySelector(".progressBar").style.width = percentage;
-  }
-  if (percentage === "50%") {
-    document.querySelector(".progressBar").style.background = "#FAD054";
-    document.querySelector(".progressBar").style.width = percentage;
-  }
-  if (percentage === "75%") {
-    document.querySelector(".progressBar").style.background = "blue";
-    document.querySelector(".progressBar").style.width = percentage;
-  }
-  if (percentage === "100%") {
-    document.querySelector(".progressBar").style.background = "green";
-    document.querySelector(".progressBar").style.width = percentage;
-  }
+  drawSecurityPercentage("25%", percentage, "red");
+  drawSecurityPercentage("50%", percentage, "#FAD054");
+  drawSecurityPercentage("75%", percentage, "blue");
+  drawSecurityPercentage("100%", percentage, "green");
 }
 
 $(document).ready(function () {
@@ -94,9 +86,9 @@ $(document).ready(function () {
     }
 
     globalStrength = getPassStrength(inputVal);
-    document.querySelector("#percent").innerHTML += "%";
+    document.querySelector("#percent").innerText += "%";
 
-    var percentage = document.querySelector("#percent").innerHTML;
+    var percentage = document.querySelector("#percent").innerText;
     percentageCheck(percentage);
   };
   document.querySelector("#password").addEventListener("keyup",
